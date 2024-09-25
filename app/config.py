5bb665d873905ed49e0caa0a3aa0b03d5fd68ae8
Prashanth Subrahmanyam <ksprashanth@google.com>
2024-09-25 20:57:35 +0530

    update to read config for domain

--- app/config.py
+++ app/config.py
@@ -26,16 +26,25 @@ class ConfigReader:
         self.domains = self.get_domains()
         self.models = self.get_models()
 
+    def _read_file(self, file_name):
+        """Reads given json config file and returns json object."""
+        
+        file_path = os.path.join(self.config_dir, file_name)
+        logger.info(f'Reading configuration file: {file_path}')
+
+        # check file exists in OS
+        if not os.path.exists(file_path):
+            logger.error(f'Configuration file not found: {file_path}')
+            raise FileNotFoundError(f'Configuration file not found: {file_path}')
+        with open(file_path, 'r') as f:
+            file_json = json.load(f)
+        return file_json
 
     def _read_config(self):
         config_data = {}
         for key, file_name in self.config_files.items():
-            file_path = os.path.join(self.config_dir, file_name)
-            logger.info(f'Reading configuration file: {file_path}')
-            if not os.path.exists(file_path):
-                raise FileNotFoundError(f'Configuration file not found: {file_path}')
-            with open(file_path, 'r') as f:
-                config_data[key] = json.load(f)
+            config_data[key] = self._read_file(file_name)
+  
         return config_data
     
     def get_domains(self):
@@ -69,3 +78,8 @@ class ConfigReader:
         return self.models[id]
     
 
+    def get_config_for_domain(self, domain):
+        """Returns the config for the given domain."""
+        domain_config = self._read_file(self.get_domain(domain)['config_file'])
+        return domain_config
+    
