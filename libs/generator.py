0eb9dcb471dae2394a8855a4c4e192139d90ba83
Prashanth Subrahmanyam <ksprashanth@google.com>
2024-09-26 07:48:40 +0530

    accumulated overall token count

--- libs/generator.py
+++ libs/generator.py
@@ -16,7 +16,11 @@ class Generator:
     reviewer = None
     config_reader = None
     domain_config = {}
-    iteration = 0
+    metadata = {
+                "total_tokens": 0,
+                "prompt_tokens": 0,
+                "candidates_tokens": 0
+            }
 
     def __init__(self, 
                  domain: str):
@@ -24,11 +28,10 @@ class Generator:
         logger.info(f"Initializing generator for domain: {domain}")
         self.config_reader = ConfigReader()
         self.domain_config = self.config_reader.get_config_for_domain(domain)
+        self._get_models()
 
-        self._get_models(domain)
 
-
-    def _get_models(self, domain: str):
+    def _get_models(self):
         """Dynamically instantiates and returns model objects"""
 
         # create writer model
@@ -67,11 +70,18 @@ class Generator:
         )
     
 
+    def _accumulate_metadata(self, metadata: object):
+        """accumulate token counts across all iterations"""
+        self.metadata['total_tokens'] += metadata['total_tokens']
+        self.metadata['prompt_tokens'] += metadata['prompt_tokens']
+        self.metadata['candidates_tokens'] += metadata['candidates_tokens']
+
+
     def generate_initial_output(self, prompt: str):
         """Generate an initial output without iterations"""
         logger.info("Get initial completion from writer")
         response = self.writer.generate_completion(prompt)
-        # TODO: Accumalate tokens
+        self._accumulate_metadata(response['usage_metadata'])
         return response
 
 
@@ -85,26 +95,31 @@ class Generator:
             self.domain_config['reviewer']['prompts']['initial_prompt'].replace(
                 _VAR_PROMPT, initial_output)
         feedback = self.reviewer.generate_completion(feedback_prompt)
+        self._accumulate_metadata(feedback['usage_metadata'])
+        iteration = 0
 
         while json.loads(feedback['output'])['recommendation'].lower() == 'revise' \
-            or self.iteration < num_iterations:
-            self.iteration += 1
-            logger.info(f"Running iteration {self.iteration}")
+            or iteration < num_iterations:
+            iteration += 1
+            logger.info(f"Running iteration {iteration}")
 
             # iterate output based on feedback
             review_prompt = \
                 self.domain_config['writer']['prompts']['iterative_prompt'].replace(
                     _VAR_PROMPT, json.loads(feedback['output'])['suggestions'])
             response = self.writer.generate_completion(review_prompt)
+            self._accumulate_metadata(response['usage_metadata'])
 
             # review revised output
             feedback_prompt = \
                 self.domain_config['reviewer']['prompts']['initial_prompt'].replace(
                 _VAR_PROMPT, response['output'])
             feedback = self.reviewer.generate_completion(feedback_prompt)
+            self._accumulate_metadata(feedback['usage_metadata'])
 
         return {
             'output': response['output'],
-            'usage_metadata': response['usage_metadata']
+            'usage_metadata': self.metadata,
+            'iterations': iteration
         }
 
